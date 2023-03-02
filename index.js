490c4379767c59b130676aec06757da66dc92fb3
Brian Davies <brian.davies589@gmail.com>
2023-03-02 11:28:16 -0500

    updated the script to allow for alternative directories: ./src/components, ../src/components, .src and ../src. the script will change directory if they exist and add components there. Throw error if they dont exist!

--- index.js
+++ index.js
@@ -3,59 +3,75 @@
 import inquirer from "inquirer";
 import fs from "fs";
 import { readFile } from "fs/promises";
+import process from "process";
 
-const html = JSON.parse(
-  await readFile(new URL("./html-lib.json", import.meta.url))
-);
-
-function validatePascalCase(str) {
-  if (!str[0].match(/[A-Z]/)) {
+try {
+  if (fs.existsSync("./src/components")) {
+    process.chdir("./src/components");
+  } else if (fs.existsSync("../src/components")) {
+    process.chdir("../src/components");
+  } else if (fs.existsSync("./src")) {
+    process.chdir("./src");
+  } else if (fs.existsSync("../src")) {
+    process.chdir("../src");
+  } else {
     throw new Error(
-      "The first word of your react component does not have a capitalized first letter and therefore is not PascalCase. Please adjust."
+      "ERROR: we were not able to locate a path of ./src or ../src or ./src/components or ../src/components and therefore cannot execute add-react-fn-component script. Please ensure one of the above paths in your project exists before retrying"
     );
-  } else return true;
-}
+  }
 
-inquirer
-  .prompt([
-    {
-      name: "componentName",
-      message: "Enter React Component Name: ",
-      validate: validatePascalCase,
-    },
-    {
-      name: "componentType",
-      type: "input",
-      message: "Which HTML element will the component return?: ",
-      default: "div",
-    },
-    {
-      name: "cssOption",
-      message: "CSS or styled-components?: ",
-      choices: ["styled-components", "css", "neither"],
-      type: "list",
-      suggestOnly: true,
-    },
-  ])
-  .then((answers) => {
-    console.log("answers:", answers);
-    if (Object.hasOwn(html, answers.componentType)) {
-      if (answers.componentName.length === 0) {
-        console.log(
-          "You must specify a component name. Pascal Case is recommended for React components."
-        );
+  const html = JSON.parse(
+    await readFile(new URL("./html-lib.json", import.meta.url))
+  );
 
-        if (answers.componentType.length === 0) {
+  function validatePascalCase(str) {
+    if (!str[0].match(/[A-Z]/)) {
+      throw new Error(
+        "The first word of your react component does not have a capitalized first letter and therefore is not PascalCase. Please adjust."
+      );
+    } else return true;
+  }
+
+  inquirer
+    .prompt([
+      {
+        name: "componentName",
+        message: "Enter React Component Name: ",
+        validate: validatePascalCase,
+      },
+      {
+        name: "componentType",
+        type: "input",
+        message: "Which HTML element will the component return?: ",
+        default: "div",
+      },
+      {
+        name: "cssOption",
+        message: "CSS or styled-components?: ",
+        choices: ["styled-components", "css", "neither"],
+        type: "list",
+        suggestOnly: true,
+      },
+    ])
+    .then((answers) => {
+      console.log("answers:", answers);
+      if (Object.hasOwn(html, answers.componentType)) {
+        if (answers.componentName.length === 0) {
           console.log(
-            "You must specify a component Type. Standard HTML ONLY!."
+            "You must specify a component name. Pascal Case is recommended for React components."
           );
-        }
-      } else {
-        fs.mkdirSync(answers.componentName);
-        if (answers.cssOption === "styled-components") {
-          fs.writeFileSync(
-            `./${answers.componentName}/${answers.componentName}.jsx`,
-            `
+
+          if (answers.componentType.length === 0) {
+            console.log(
+              "You must specify a component Type. Standard HTML ONLY!."
+            );
+          }
+        } else {
+          fs.mkdirSync(answers.componentName);
+          if (answers.cssOption === "styled-components") {
+            fs.writeFileSync(
+              `./${answers.componentName}/${answers.componentName}.jsx`,
+              `
 import styled from 'styled-components';
               
 export default function ${answers.componentName}() {
@@ -67,44 +83,47 @@ export default function ${answers.componentName}() {
                       
 const Styled${answers.componentName} = styled.${answers.componentType}\`\`;
 `
-          );
-          console.log(
-            `Component ${answers.componentName} created successfully with styled-component boiler-plate!`
-          );
-        }
-
-        if (answers.cssOption === "css") {
-          let className = "";
-
-          for (let i = 0; i < answers.componentName.length; i++) {
-            console.log("answers.componentName[i]:", answers.componentName[i]);
-            // is letter a capital?
-            if (answers.componentName[i].match(/[A-Z]/)) {
-              // if 1st char in str, no hyphen, else hyphen
-              i === 0
-                ? (className += answers.componentName[i])
-                : (className += "-" + answers.componentName[i]);
-            } else {
-              className += answers.componentName[i];
-            }
+            );
+            console.log(
+              `Component ${answers.componentName} created successfully with styled-component boiler-plate!`
+            );
           }
 
-          const finalClassName = className.toLowerCase();
+          if (answers.cssOption === "css") {
+            let className = "";
 
-          console.log("className:", finalClassName);
-          console.log("finalClassName:", finalClassName);
+            for (let i = 0; i < answers.componentName.length; i++) {
+              console.log(
+                "answers.componentName[i]:",
+                answers.componentName[i]
+              );
+              // is letter a capital?
+              if (answers.componentName[i].match(/[A-Z]/)) {
+                // if 1st char in str, no hyphen, else hyphen
+                i === 0
+                  ? (className += answers.componentName[i])
+                  : (className += "-" + answers.componentName[i]);
+              } else {
+                className += answers.componentName[i];
+              }
+            }
 
-          fs.writeFileSync(
-            `./${answers.componentName}/${answers.componentName}.css`,
-            `.${finalClassName} {
+            const finalClassName = className.toLowerCase();
+
+            console.log("className:", finalClassName);
+            console.log("finalClassName:", finalClassName);
+
+            fs.writeFileSync(
+              `./${answers.componentName}/${answers.componentName}.css`,
+              `.${finalClassName} {
 display: visible;
 /* enter css here */
 }
 `
-          );
-          fs.writeFileSync(
-            `./${answers.componentName}/${answers.componentName}.jsx`,
-            `import '${answers.componentName}.css';
+            );
+            fs.writeFileSync(
+              `./${answers.componentName}/${answers.componentName}.jsx`,
+              `import '${answers.componentName}.css';
               
 export default function ${answers.componentName}() {
     return (
@@ -113,14 +132,20 @@ export default function ${answers.componentName}() {
     );
 }
 `
-          );
-          console.log(
-            `Component ${answers.componentName} created successfully with css boiler-plate!`
-          );
+            );
+            console.log(
+              `Component ${answers.componentName} created successfully with css boiler-plate!`
+            );
+          }
         }
-      }
-    } else
-      throw new Error(
-        `the html element you entered: ${answers.componentType} is NOT VALID. Please check your spelling`
-      );
-  });
+      } else
+        throw new Error(
+          `the html element you entered: ${answers.componentType} is NOT VALID. Please check your spelling`
+        );
+    });
+} catch (error) {
+  console.error(
+    "NO SRC:",
+    "not able to locate a /src directory in the current project"
+  );
+}
